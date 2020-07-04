38147ba9e62a7ae616bae70d3d891072d48956b3
rwdperez <rwdperez@gmail.com>
2020-07-04 17:11:23 -0300

    Ejercicio 4 con codigo desde 0

---
+++ Ejercicio4Nuevo/main.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include <cstdio>
+
+
+using namespace std;
+
+class Juego {
+	
+	private:
+		
+		int mapa[3][3];
+		char letra;
+		
+		
+	
+	
+	public:
+		
+		Juego();
+		
+		void SetMapa(int x, int y, char c);
+		
+		void Mostrar();
+	
+};
+
+Juego::Juego(){}
+
+void Juego::SetMapa(int x, int y, char c){
+	letra = letra;
+	mapa[x][y] = letra;
+	
+
+}
+
+
+void Juego::Mostrar(){
+	
+	for (int i = 0; i < 3; i++){
+		for (int j = 0; j < 3; j++){
+			
+			cout<<mapa[i][j]<<" |";
+		}
+		cout<<endl;
+	}
+}
+
+
+
+
+int main (int argc, char *argv[]) {
+	
+	
+	
+	Juego Jugador1;
+	Jugador1.SetMapa(0,0,'X');
+	Jugador1.Mostrar();
+	
+	
+	
+	
+	
+	return 0;
+}
+
